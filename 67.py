016cc8185d73e17321f75a8e232d602486e6b5df
kastyan2007 <106815545+kastyan2007@users.noreply.github.com>
2026-03-14 17:49:55 +0300

    Update 67.py

--- 67.py
+++ 67.py
@@ -2,50 +2,46 @@ from .. import loader, utils
 import asyncio
 
 @loader.tds
-class SixSevenMod(loader.Module):
-    """Анимация .67"""
+class SixSeven(loader.Module):
+    """Анимация 67"""
 
     strings = {"name": "SixSeven"}
 
-    async def cmd67(self, message):
-        """Запусти анимацию"""
-        
-        frames = [
-"""
-⠀⠀⠀⠀⢀⣀⣀
-⠀⠀⠀⢠⣿⣿⣿⣧
-⠀⠀⠀⣿⣿⣿⣿⣿
-⠀⠀⠀⠈⠻⣿⠟
-⠀⠀⠀⢠⡟⢻⡄
-⠀⠀⠀⠀⠀⢸⡇
-⠀⠀⠀⠀⠀⠘⠃
-   67 👋
-""",
-"""
-⠀⠀⠀⠀⢀⣀⣀
-⠀⠀⠀⢠⣿⣿⣿⣧
-⠀⠀⠀⣿⣿⣿⣿⣿
-⠀⠀⠀⠈⠻⣿⠟
-⠀⠀⠀⢸⡇⢸⡇
-⠀⠀⠀⠀⢳⡟
-⠀⠀⠀⠀⠘
-   67 👋
-""",
-"""
-⠀⠀⠀⠀⢀⣀⣀
-⠀⠀⠀⢠⣿⣿⣿⣧
-⠀⠀⠀⣿⣿⣿⣿⣿
-⠀⠀⠀⠈⠻⣿⠟
-⠀⠀⠀⢸⡇⢸⡇
-⠀⠀⠀⢸⡇⠘⣧
-⠀⠀⠀⠀⠀⠀⠘
-   67 👋
-"""
-        ]
-
+    async def _animate(self, message, frames):
         msg = await utils.answer(message, frames[0])
-
-        for _ in range(6):
+        for _ in range(5):
             for frame in frames:
                 await asyncio.sleep(0.4)
                 await msg.edit(frame)
+
+    async def 67cmd(self, message):
+        """анимация .67"""
+
+        frames = [
+"""
+   ⠀⢀⣀
+  ⢠⣿⣿⣧
+  ⣿⣿⣿⣿
+   ⠻⣿⠟
+   ⢸⡇
+  👋 67
+""",
+"""
+   ⠀⢀⣀
+  ⢠⣿⣿⣧
+  ⣿⣿⣿⣿
+   ⠻⣿⠟
+   ⢸⡇⣀
+      👋
+""",
+"""
+   ⠀⢀⣀
+  ⢠⣿⣿⣧
+  ⣿⣿⣿⣿
+   ⠻⣿⠟
+   ⣸⡇
+   👋 67
+"""
+        ]
+
+        await self._animate(message, frames)
